b3a3ba9a8f3db0d2dd14e41be3db96940cb83397
sheng0730 <6612sheng6612@gmail.com>
2025-05-02 15:41:34 +0800

    feat: textinput button

--- src/components/home/tilt.tsx
+++ src/components/home/tilt.tsx
@@ -1,7 +1,9 @@
+import { Button } from "../ui/button";
+
 export default function Hometitle() {
     return (
         <div className="p-4">
-            <div className="text-2xl text-gray-600 flex justify-between border-b-4 mb-2">
+            <div className="text-2xl text-gray-600 flex justify-between border-b-4 border-gray-500 mb-2">
                 <p className="font-bold text-black">notebook</p>
                 <div className="flex space-x-4 text-2xl font-bold">
                     <div>button 1</div>
@@ -10,9 +12,20 @@ export default function Hometitle() {
                 </div>
             </div>
             <div className="flex h-svh">
-                <div className="w-1/4 p-4 border-r-2">text</div>
-                <div className="w-3/4 p-4 border-r-2">text</div>
+                <div className="w-1/4 p-4 border-r-2">
+                    <div>sidebr</div>
+                </div>
+                <div className="w-3/4 p-4 border-r-2">
+                <label className="flex-grow p-4 border-b-2 border-gray-300">新增日記</label>
+                    <div className="flex justify-between">
+                    <textarea
+                    className="flex-grow p-2 border border-gray-300 resize text-left"
+                    placeholder="Enter text"
+                    rows={4}/>
+                    </div>
+                <Button className="flex " variant="secondary">確認</Button>
+                </div>
             </div>
         </div>
     );
-}
+}//TODO：新增sidebar  與 button 
