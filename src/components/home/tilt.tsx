8a9c366a6e7914371bbb2b38d1f750c64a852c3f
sheng0730 <6612sheng6612@gmail.com>
2025-05-06 11:53:58 +0800

    null

--- src/components/home/tilt.tsx
+++ src/components/home/tilt.tsx
@@ -1,11 +1,12 @@
 import { Button } from "../ui/button";
 import Link from "next/link";
+import Homecard from "./card";
  
 export default function Hometitle() {
     return (
         <div className="p-4">
-            <div className="text-2xl text-gray-600 flex justify-between border-b-4 border-gray-500 mb-2">
-                <p className="font-bold text-black">notebook</p>
+            <div className="text-2xl text-gray-600 flex justify-between border-b-4  mb-2">
+                <p className="font-bold text-blak">notebook</p>
                 <div className="flex space-x-4 text-2xl font-bold">
                     <div className="">
                         <Link href="/login/page">
@@ -17,18 +18,20 @@ export default function Hometitle() {
                 </div>
             </div>
             <div className="flex h-svh">
-                <div className="w-1/4 p-4 border-r-2">
+                <div className=" w-5/1 p-4 border-r-2">
                     <div>sidebr</div>
                 </div>
-                <div className="w-3/4 p-4 border-r-2">
-                <label className="flex-grow p-4 border-b-2 border-gray-300">新增日記</label>
+                <div className="">
+
+                <Homecard/>
+                {/* <label className="flex-grow p-4 border-b-2 border-gray-300">新增日記</label>
                     <div className="flex justify-between">
                     <textarea
                     className="flex-grow p-2 border border-gray-300 resize text-left"
                     placeholder="Enter text"
                     rows={4}/>
                     </div>
-                <Button className="flex " variant="secondary">確認</Button>
+                <Button className="flex " variant="secondary">確認</Button> */}
                 </div>
             </div>
         </div>
