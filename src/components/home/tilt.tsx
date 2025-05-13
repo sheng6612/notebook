e28ed66a3c2e770ddb6c067f6eea4d31065716a3
sheng0730 <6612sheng6612@gmail.com>
2025-05-13 16:23:22 +0800

    feat: add button to card
    fix link

--- src/components/home/tilt.tsx
+++ src/components/home/tilt.tsx
@@ -1,30 +1,34 @@
 import { Button } from "../ui/button";
 import Link from "next/link";
-import Homecard from "./card";
+
  
 
 export default function Hometitle() {
     return (
         <div className="p-4">
             <div className="text-2xl text-gray-600 flex justify-between border-b-4  mb-2">
-                <p className="font-bold text-blak">notebook</p>
+                <div className="font-bold text-blak">notebook</div>
                 <div className="flex space-x-4 text-2xl font-bold">
                     <div className="">
-                        <Link href="/login/page">
+                        <Link href="/login">
                             <Button variant="outline" className="text-2xl">登入</Button>
                         </Link>                            
                     </div>
-                    <div>button 2</div>
+                    <div>
+                        <Link href="/card">
+                            <Button variant="outline" className="text-2xl font-bold">備忘錄</Button>
+                        </Link>
+                    </div>
                     <div>button 3</div>
                 </div>
             </div>
             <div className="flex h-svh">
-                <div className=" w-5/1 p-4 border-r-2">
+                <div  id="index" className=" w-5/1 p-4 border-r-2">
                     <div>sidebr</div>
                 </div>
                 <div className="">
-
-                <Homecard/>
+              
+                
                 {/* <label className="flex-grow p-4 border-b-2 border-gray-300">新增日記</label>
                     <div className="flex justify-between">
                     <textarea
@@ -37,5 +41,5 @@ export default function Hometitle() {
             </div>
         </div>
     );
-}//TODO：新增sidebar 
-//FIXME: homecard
+}//TODO：新增sidebar 主畫面 
+//FIXME: card into div
