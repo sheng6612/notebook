6244755524565156971fd715ba9fa397b1c5527a
sheng0730 <6612sheng6612@gmail.com>
2025-05-16 11:15:59 +0800

    fix: exchange to app/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,37 @@
+//import Hometitle from "@/components/home/tilt";
 
-
-import Hometitle from "@/components/home/tilt";
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function HomePage(){
   return(
-    <div className="flex h-screen w-full bg-gradient-to-b from-slate-200 to-slate-500">
-      <div className="w-full"><Hometitle/></div>
+    <div className=" h-screen w-full bg-gradient-to-b from-slate-200 to-slate-500">
+       <div className="">
+            <div className="text-2xl text-gray-600 flex justify-between border-b-4  mb-2">
+                <div className="font-bold text-blak">notebook</div>
+                <div className="flex space-x-4 text-2xl font-bold">
+                    <div className="">
+                        <Link href="/hook/login">
+                            <Button variant="outline" className="text-2xl">登入</Button>
+                        </Link>                            
+                    </div>
+                    <div>
+                        <Link href="/hook/card">
+                            <Button variant="outline" className="text-2xl font-bold">備忘錄</Button>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link href="/hook/journal">
+                        <Button variant="outline" className="text-2xl font-bold">日記</Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="">
+            <p className="text-4xl text-center">index</p>
+            </div>
+        </div>
     </div>
   )
-}
+}//TODO：新增sidebar 主畫面設計 
+//FIXME: card into div
