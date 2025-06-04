fca7b3d7a5b328cd7cd6136af457b821c86a68b6
sheng0730 <6612sheng6612@gmail.com>
2025-06-04 23:44:44 +0800

    null

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -178,7 +178,7 @@ export default function IntegratedCardDrawingTool() {
         
         {cards.map(card => (
           <div key={card.id}>
-           
+          
             {editingCardId === card.id && (
                     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
                         <EditCard
